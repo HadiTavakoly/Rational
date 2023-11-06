eb09e06f73b7e44f133502bf466451086020cc5a
Your Name <hadi.tavakoli.000@gmail.com>
2023-11-06 19:31:32 +0330

    create Rational project

---
+++ src/Main.java
@@ -0,0 +1,52 @@
+public class Main {
+    public static void main(String[] args) {
+        Rational rational1 =new Rational(5,6);
+        Rational rational2 =new Rational(9,5);
+        System.out.println("rational1 : "+rational1.toString());
+        System.out.println("rational2 : "+rational2.toString());
+        System.out.println("-------------------------------------------");
+//        rational1.compareTo(rational2);
+//        Rational add = rational1.add(rational2);
+        System.out.println(rational1.compareTo(rational2));
+        rational1.findMin(new Rational(-1,6));
+        rational1.findMax(new Rational(-1,6));
+//        Rational sub = rational1.subtract(rational2);
+//        System.out.println(sub.toString());
+//        Rational mul = rational1.multiply(rational2);
+//        System.out.println(mul.toString());
+//        Rational mulBy = rational1.multiplyBy(5);
+//        System.out.println(mulBy.toString());
+//        Rational div = rational1.divide(rational2);
+//        System.out.println(mulBy.toString());
+//        Rational divBy = rational1.divideBy(5);
+//        System.out.println(divBy.toString());
+//        Rational inc = rational1.increment();
+//        System.out.println(inc.toString());
+//        Rational incBy = rational1.incrementBy(3);
+//        System.out.println(incBy.toString());
+//        Rational dec = rational1.decrement();
+//        System.out.println(dec.toString());
+//        Rational decBy = rational1.decrementBy(4);
+//        System.out.println(decBy.toString());
+//        Rational abs = rational2.abs();
+//        System.out.println(abs.toString());
+//        int sig = rational2.signum();
+//        System.out.println("Signum : "+sig);
+//        Rational neg = rational2.negate();
+//        System.out.println(neg.toString());
+//        Rational rec = rational1.reciprocal();
+//        System.out.println(rec.toString());
+//        Rational pow =  rational1.pow(3);
+//        System.out.println(pow.toString());
+//        System.out.println("6/10 : "+rational1.intValue());
+//        System.out.println("6/10 : "+rational1.longValue());
+//        System.out.println("6/10 : "+rational1.doubleValue());
+//        System.out.println("6/10 : "+rational1.floatValue());
+//        rational1.printRational();
+//        rational1.showRational();
+//        rational2.isProper();
+//        rational1.toMixed();
+//        rational1.equals(rational2);
+
+    }
+}
---
+++ src/Rational.java
@@ -0,0 +1,350 @@
+public class Rational {
+    private int num;
+    private int den;
+
+    //Default Constructor
+    public Rational(){
+        this.num=1;
+        this.den=1;
+    }
+
+    //One-parameter constructor
+    public Rational(int num){
+        this.num=num ;
+        this.den=1;
+    }
+
+    //Two-parameter constructor
+    public Rational(int num,int den){
+        this.num=num ;
+        this.den=den ;
+//        simplify();
+    }
+
+    //Copy constructor
+    public Rational(Rational rational){
+        this.num= rational.num;
+        this.den= rational.den;
+    }
+
+    //GCD
+    public int gcd(int a,int b){
+        int i;
+        if(a<b)
+            i = a;
+        else
+            i=b;
+        for (i = i; i > 1; i--) {
+
+            if (a % i == 0 && b % i == 0)
+                return i;
+        }
+        return 1;
+    }
+
+    //Simplify method
+    private void simplify(){
+        if(num>0 && den>0) {
+            int gcdNum = gcd(this.num, this.den);
+            this.num = num / gcdNum;
+            this.den = den / gcdNum;
+        } else if (num<0 && den<0) {
+            int gcdNum = gcd(this.num*(-1), this.den*(-1));
+            this.num = num*(-1) / gcdNum;
+            this.den = den*(-1) / gcdNum;
+        } else if (num<0 && den>0) {
+            int gcdNum = gcd(this.num*(-1), this.den);
+            this.num = num / gcdNum;
+            this.den = den / gcdNum;
+        }else {
+            int gcdNum = gcd(this.num, this.den*(-1));
+            this.num = num*(-1) / gcdNum;
+            this.den = den*(-1) / gcdNum;
+        }
+    }
+
+    //Get numerator value
+    public int getNum() {
+        return num;
+    }
+
+    //Get denominator value
+    public int getDen() {
+        return den;
+    }
+
+    //Add method
+    public Rational add(Rational rational) {
+        System.out.print(this.toString()+" + "+rational.toString()+" = ");
+        Rational result = new Rational((this.num*rational.den)+(rational.num*this.den),this.den*rational.den);
+        result.simplify();
+        return result;
+    }
+
+    //Subtract method
+    public Rational subtract(Rational rational) {
+        System.out.print(this.toString()+" - "+rational.toString()+" = ");
+        Rational result = new Rational((this.num*rational.den)-(rational.num*this.den),this.den*rational.den);
+        result.simplify();
+        return result;
+
+    }
+
+    //Multiply method
+    public Rational multiply(Rational rational) {
+        System.out.print(this.toString()+" * "+rational.toString()+" = ");
+        Rational result = new Rational(this.num*rational.num,this.den*rational.den);
+        result.simplify();
+        return result;
+    }
+
+    //MultiplyBy method
+    public Rational multiplyBy(int number) {
+        System.out.print(this.toString()+" * "+number+" = ");
+        Rational result = new Rational(this.num*number,this.den);
+        result.simplify();
+        return result;
+    }
+
+    //Divide method
+    public Rational divide(Rational rational) {
+        System.out.print(this.toString()+" / "+rational.toString()+" = ");
+        Rational result =  new Rational(this.num*rational.den,this.den*rational.num);
+        result.simplify();
+        return result;
+    }
+
+    //DivideBy method
+    public Rational divideBy(int number) {
+        System.out.print(this.toString()+" / "+number+" = ");
+        Rational result =  new Rational(this.num,this.den*number);
+        result.simplify();
+        return result;
+    }
+
+    //Increment method
+    public Rational increment() {
+        Rational result =  new Rational(this.num,this.den).add(new Rational(1));
+        result.simplify();
+        return result;
+    }
+
+    //IncrementBy  method
+    public Rational incrementBy(int number) {
+        Rational result =  new Rational(this.num,this.den).add(new Rational(number));
+        result.simplify();
+        return result;
+    }
+
+    //Decrement method
+    public Rational decrement() {
+        Rational result =  new Rational(this.num,this.den).subtract(new Rational(1));
+        result.simplify();
+        return result;
+    }
+
+    //DecrementBy method
+    public Rational decrementBy(int number) {
+        Rational result =  new Rational(this.num,this.den).subtract(new Rational(number));
+        result.simplify();
+        return result;
+    }
+
+    //Abs method
+    public Rational abs() {
+        Rational result =  new Rational(Math.abs(this.num),Math.abs(this.den));
+        result.simplify();
+        return result;
+    }
+
+    //Signum method
+    public int signum() {
+        if(num>0 && den>0 || num<0 && den<0) {
+            return 1;
+        } else {
+        return -1;
+        }
+    }
+
+    //Negate method
+    public Rational negate() {
+        Rational result = new Rational((-1)*this.num,this.den);
+        result.simplify();
+        return result;
+    }
+
+    //Reciprocal method
+    public Rational reciprocal() {
+        Rational result = new Rational(this.den,this.num);
+        result.simplify();
+        return result;
+    }
+
+    //Pow method
+    public Rational pow(int powNum) {
+        Rational result = new Rational((int) Math.pow(this.num,powNum),(int) Math.pow(this.den,powNum) );
+        result.simplify();
+        return result;
+    }
+
+    //IntValue method
+    public String intValue() {
+        return ((int) this.num+"/"+(int) this.den);
+    }
+
+    //LongValue method
+    public String longValue() {
+        return ((long) this.num+"/"+(long) this.den);
+    }
+
+    //floatValue method
+    public String floatValue() {
+        return ((float) this.num+"/"+(float) this.den);
+    }
+
+    //doubleValue method
+    public String doubleValue() {
+        return ((double) this.num+"/"+(double) this.den);
+    }
+
+
+    @Override
+    public String toString() {
+        if (this.den==1)
+            return ""+this.num;
+        else
+            return this.num+"/"+this.den;
+
+    }
+
+    //PrintRational method
+    public void printRational() {
+//        if (this.den==1)
+//            System.out.println(this.num) ;
+//        else
+            System.out.println(this.num+"/"+this.den) ;
+    }
+
+    //ShowRational method
+    public void showRational() {
+        if (this.den==1)
+            System.out.println("Rational("+this.num+")") ;
+        else
+            System.out.println("Rational("+this.num+","+this.den+")") ;
+    }
+
+    //IsProper method
+    public void isProper() {
+        //
+        //simplify();
+        //
+        String i = this.num<=this.den? "True" : "False";
+        System.out.println(i);
+    }
+
+    //IsImproper method
+    public void isImproper() {
+        //
+        //simplify();
+        //
+        String i = this.num>this.den? "True" : "False";
+        System.out.println(i);
+    }
+
+    //ToMixed method
+    public void toMixed() {
+        simplify();
+        if(this.num>this.den)
+            System.out.println((int)this.num/this.den +" "+decrementBy(2));
+    }
+
+    //Equals method
+    public void equals(Rational rational) {
+        simplify();
+        rational.simplify();
+        if(this.num==rational.num && this.den==rational.den)
+            System.out.println("True");
+        else
+            System.out.println("False");
+    }
+
+    //CompareTo method
+    public int compareTo(Rational rational) {
+        int i=0;
+        simplify();
+        rational.simplify();
+        if(this.num==rational.num && this.den==rational.den)
+            i= 0;
+//            System.out.println("Equals");
+        else if (this.den==rational.den && this.num>rational.num)
+            i= 1;
+//            System.out.println("Bigger");
+        else if (this.den==rational.den && this.num<rational.num)
+            i= -1;
+//            System.out.println("Smaller");
+        else if (this.num==rational.num && this.den>rational.den)
+            i= -1;
+//            System.out.println("Smaller");
+        else if (this.num==rational.num && this.den<rational.den)
+            i= 1;
+//            System.out.println("Bigger");
+        else {
+            int gcdNum = gcd(this.den, rational.den);
+            int lcmNum = (this.den* rational.den)/gcdNum;
+
+            int z1 = lcmNum/this.den;
+            int z2 = lcmNum/rational.den;
+            this.num=this.num*z1;
+            this.den=this.den*z1;
+            rational.num=rational.num*z2;
+            rational.den=rational.den*z2;
+            if(this.num==rational.num && this.den==rational.den)
+                i= 0;
+//                System.out.println("Equals");
+            else if (this.den==rational.den && this.num>rational.num)
+                i= 1;
+//                System.out.println("Bigger");
+            else if (this.den==rational.den && this.num<rational.num)
+                i= -1;
+//                System.out.println("Smaller");
+            else if (this.num==rational.num && this.den>rational.den)
+                i= -1;
+//                System.out.println("Smaller");
+            else if (this.num==rational.num && this.den<rational.den)
+                i= 1;
+//                System.out.println("Bigger");
+        }
+        return i;
+
+    }
+
+    //FindMin method
+    public void findMin(Rational rational) {
+        int i;
+        i = compareTo(rational);
+        if (i < 1){
+            simplify();
+            printRational();
+        }
+        else {
+            rational.simplify();
+            rational.printRational();
+        }
+    }
+
+    //FindMax method
+    public void findMax(Rational rational) {
+        int i;
+        i = compareTo(rational);
+        if (i >= 1){
+            simplify();
+            printRational();
+        }
+        else {
+            rational.simplify();
+            rational.printRational();
+        }
+    }
+
+
+}
